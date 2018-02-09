a2413a3193c374b2e2ba3966d09d9f5b0fdf09db
Berstad <tor.berstad@gmail.com>
2018-02-09 12:22:32 +0100

    Changed project structure signifagantly and fixed errors. - Server.py now runs successfully using the combined dataset and test parameters. - Metrics tests run and print a few metrics. - Created a few documentation files.

---
+++ server/network/keras_net.py
@@ -0,0 +1,176 @@
+__authors__ = ["Tor Jan Derek Berstad", "Michael Riegler"]
+__copyright__ = "Tor Jan Derek Berstad"
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Tor Jan Derek Berstad"
+__email__ = "tjbersta@ifi.uio.no"
+__status__ = "Development"
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.md', which is part of this source code package.
+
+from keras.applications.xception import Xception
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint
+from collections import Counter
+import os
+from keras.callbacks import EarlyStopping
+import sys
+
+class KerasNet:
+    base_model = False
+    verbose = False
+    predictions = False
+    model = False
+    train_datagen = False
+    validation_datagen = False
+    train_generator = False
+    callbacks_list = False
+    paramdict = False
+    setup_completed = False
+    top_weights_path = ""
+    final_weights_path = ""
+    train_data_dir = ""
+    validation_data_dir = ""
+    model_dir = ""
+
+
+
+    def __init__(self,paramdict_in,verbose_in = False):
+        #the base model
+        self.verbose = verbose_in
+        self.paramdict = paramdict_in
+        try:
+            self.base_model = Xception(input_shape=(self.paramdict['imagedims'][0], self.paramdict['imagedims'][1], 3),
+                                  weights='imagenet', include_top=False)
+            # Top Model Block
+            x = self.base_model.output
+            x = GlobalAveragePooling2D()(x)
+            self.predictions = Dense(self.paramdict['nb_classes'], activation=self.paramdict['activation'])(x)
+
+            # add your top layer block to your base model
+            self.model = Model(self.base_model.input, self.predictions)
+
+            self.train_data_dir = os.path.dirname(os.path.abspath(__file__)) + self.paramdict["train_data_dir"]
+            self.validation_data_dir = os.path.dirname(os.path.abspath(__file__)) + self.paramdict["validation_data_dir"]
+            self.model_dir = os.path.dirname(os.path.abspath(__file__)) + self.paramdict["model_dir"]
+
+            if self.verbose:
+                print(self.model.summary())
+                print("Initilialization complete")
+                self.setup_completed = True
+        except:
+            e = sys.exc_info()[0]
+            print("Error: ",e)
+
+
+    #weight function for imbalanced datasets
+    def get_class_weights(self,y):
+        counter = Counter(y)
+        if self.verbose:
+            print(counter)
+        majority = max(counter.values())
+        return  {cls: float(majority/count) for cls, count in counter.items()}
+
+        # # let's visualize layer names and layer indices to see how many layers/blocks to re-train
+        # # uncomment when choosing based_model_last_block_layer
+        # for i, layer in enumerate(model.layers):
+        #     print(i, layer.name)
+
+    def gen_data(self,save_preview = False):
+        # first: train only the top layers (which were randomly initialized)
+        # i.e. freeze all layers of the based model that is already pre-trained.
+        if self.verbose:
+            print("Generating data")
+        for layer in self.base_model.layers:
+            layer.trainable = False
+
+        # Read Data and Augment it: Make sure to select augmentations that are appropriate to your images.
+        # To save augmentations un-comment save lines and add to your flow parameters.
+        self.train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                           #rotation_range=transformation_ratio,
+                                           #shear_range=transformation_ratio,
+                                           #zoom_range=transformation_ratio,
+                                           #cval=transformation_ratio,
+                                           horizontal_flip=True,
+                                           vertical_flip=True)
+        self.validation_datagen = ImageDataGenerator(rescale=1. / 255)
+        self.train_generator = self.train_datagen.flow_from_directory(self.train_data_dir,
+                                                            target_size=(self.paramdict['imagedims'][0],
+                                                                         self.paramdict['imagedims'][1]),
+                                                            batch_size=self.paramdict['batch_size'],
+                                                            class_mode='categorical')
+
+        # use the above 3 commented lines if you want to save and look at how the data augmentations look like
+        # TODO: Look at this, it seems wrong
+        if save_preview:
+            os.makedirs(os.path.join(self.train_data_dir, '../preview'), exist_ok=True)
+            self.model.save_to_dir(os.path.join(self.train_data_dir, '../preview'), save_prefix='aug', save_format='jpeg')
+
+        self.validation_generator = self.validation_datagen.flow_from_directory(self.validation_data_dir,
+                                                                                target_size=(self.paramdict['imagedims'][0],
+                                                                                             self.paramdict['imagedims'][1]),
+                                                                                batch_size=self.paramdict['batch_size'],
+                                                                                class_mode='categorical')
+
+    def compile_setup(self):
+        # Compile the model
+        self.model.compile(self.paramdict['optimizer'],
+                      loss=self.paramdict['loss'],  # categorical_crossentropy if multi-class classifier
+                      metrics=['accuracy'])
+
+        self.top_weights_path = os.path.join(self.model_dir, 'top_model_weights.hdf5')
+        self.callbacks_list = [
+            ModelCheckpoint(self.top_weights_path, monitor='val_acc', verbose=1, save_best_only=True),
+            EarlyStopping(monitor='val_acc', patience=5, verbose=0)
+        ]
+
+    def train(self):
+        # Train Simple CNN
+        self.model.fit_generator(self.train_generator,
+                                 steps_per_epoch=self.paramdict['nb_train_samples'] // self.paramdict['batch_size'],
+                                 epochs=self.paramdict['nb_epoch'] / 5,
+                                 validation_data=self.validation_generator,
+                                 validation_steps=self.paramdict['nb_validation_samples'] // self.paramdict['batch_size'],
+                                 callbacks=self.callbacks_list)
+
+
+    # fine-tune the model
+    def fine_tune(self):
+        # add the best weights from the train top model
+        # at this point we have the pre-train weights of the base model and the trained weight of the new/added top model
+        # we re-load model weights to ensure the best epoch is selected and not the last one.
+        self.model.load_weights(self.top_weights_path)
+
+        # based_model_last_block_layer_number points to the layer in your model you want to train.
+        # For example if you want to train the last block of a 19 layer VGG16 model this should be 15
+        # If you want to train the last Two blocks of an Inception model it should be 172
+        # layers before this number will used the pre-trained weights, layers above and including this number
+        # will be re-trained based on the new data.
+        for layer in self.model.layers[:self.paramdict["based_model_last_block_layer_number"]]:
+            layer.trainable = False
+        for layer in self.model.layers[self.paramdict["based_model_last_block_layer_number"]:]:
+            layer.trainable = True
+
+        # compile the model with a SGD/momentum optimizer
+        # and a very slow learning rate.
+        self.model.compile(optimizer='nadam',
+                           loss='categorical_crossentropy',
+                           metrics=['accuracy'])
+
+        # save weights of best training epoch: monitor either val_loss or val_acc
+        self.final_weights_path = os.path.join(self.model_dir, 'model_weights.hdf5')
+        self.callbacks_list = [
+            ModelCheckpoint(self.final_weights_path, monitor='val_acc', verbose=1, save_best_only=True),
+            EarlyStopping(monitor='val_loss', patience=5, verbose=0)
+        ]
+        self.model.fit_generator(self.train_generator,
+                                steps_per_epoch=self.paramdict['nb_train_samples'] // self.paramdict['batch_size'],
+                                epochs=self.paramdict['nb_epoch'],
+                                validation_data=self.validation_generator,
+                                validation_steps=self.paramdict['nb_validation_samples'] // self.paramdict['batch_size'],
+                                callbacks=self.callbacks_list)
+
+if __name__ == '__main__':
+    print("This program is not meant to be run as is, run using server.py as wrapper")
--- server/server.py
+++ server/server.py
@@ -1,14 +1,87 @@
 #source \virtualenv\TFmetrics\bin\activate
+
+__authors__ = ["Tor Jan Derek Berstad"]
+__copyright__ = "Tor Jan Derek Berstad"
+__credits__ = ["Michael Riegler"]
+__license__ = "MIT"
+__version__ = "0.1"
+__maintainer__ = "Tor Jan Derek Berstad"
+__email__ = "tjbersta@ifi.uio.no"
+__status__ = "Development"
+# This file is subject to the terms and conditions defined in
+# file 'LICENSE.md', which is part of this source code package.
+
 from flask import Flask
+import json
+import os
+import metrics.met_nvidia as nvmet
+import metrics.met_psutil as psmet
+import metrics.met_tplink as tpmet
 
 app = Flask(__name__)
 
 @app.route('/')
-def import_network(path):
 
 
+@app.route('/network/<networkname>', methods=['GET', 'POST'])
+def network_admin(networkname):
+    # Administrate the network with the given name
+    # GET to this should return current information about the network
+    # GET with payload is defined in docs for API
+    pass
 
+
+# Hyper parameters for model
+def get_params(network="multiclass", database = False):
+    # nb_classes = number of classes 2 for binary, >2 for multiclass, has to be equal to the folder in the train and
+    # based_model_last_block_layer_number = value is based on based model selected.
+    # imagedims = change based on the shape/structure of your images
+    # batch_size = try 4, 8, 16, 32, 64, 128, 256 dependent on CPU/GPU memory capacity (powers of 2 values).
+    # nb_epoch = number of iteration the algorithm gets trained.
+    # learn_rate = sgd learning rate
+    # momentum = sgd momentum to avoid local minimum
+    # transformation_ratio = how aggressive will be the data augmentation/transformation
+    # optimizer = Optimizer to use for the model, nadam/adam or similar
+    # loss = loss type for the model
+    # ALL PATHS ARE RELATIVE TO THE CURRENT FILE (server.py)
+    # train_data_dir = training data directory, one folder for each class
+    # validation_data_dir = validation data directory, one folder for each class
+    # model_dir = path to store the finished models, must exist.
+    # nb_train_samples = samples in train data
+    # nb_validation_samples = samples in validation data
+    # TODO: Fix the paths here using os.path or similar
+    paramdict = {}
+    if not database:
+        if network == "multiclass":
+            with open(os.path.dirname(os.path.abspath(__file__)) +
+                      "/network/templates/parameters/params_multiclass.json") as json_data:
+                paramdict = json.load(json_data)
+        elif network == "binary":
+            with open(os.path.dirname(os.path.abspath(__file__)) +
+                      "/network/templates/parameters/params_binary.json") as json_data:
+                paramdict = json.load(json_data)
+    else:
+        print("Not yet implemented")
+        # TODO: Implement a function to retrieve the parameters from the database
+
+    return paramdict
+
+
+def setup_network(network_path):
+    from network.keras_net import KerasNet
+    params = get_params()
+    print(params)
+    net = KerasNet(params,True)
+    if net.setup_completed:
+        net.gen_data()
+        net.compile_setup()
+    return net
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+
+    net = setup_network("/network/keras_net.py")
+    if net.setup_completed:
+        net.train()
+        net.fine_tune()
